220e37cd99fae632518adc3c1f7a4fdfe7a6caed
Xaviju <xaviju@gmail.com>
2019-07-17 11:40:31 +0200

    Query Builder

--- akcidento-api/controller/accidents.controller.js
+++ akcidento-api/controller/accidents.controller.js
@@ -1,20 +1,46 @@
 const db = require('../config/db.config.js');
-const AccidentsBySexSect = db.accidents_by_sexsect;
-const AccidentsByContract = db.accidents_by_contract;
+const accidentsBySexSect = db.accidents_by_sexsect;
+const accidentsByContract = db.accidents_by_contract;
 
 exports.handleRequest = (req, res) => {
     const criteria = req.params.groupCriteria;
-    AccidentsByContract.findAll({
-        attributes: [
-            [db.sequelize.fn('SUM', db.sequelize.col('total')), 'total'],
-            'year',
-            'modality_id',
-        ],
-        group: ['year', criteria],
-        order: ['year', criteria]
-    }).then((accidents) => {
-        res.send(accidents);
-    }).catch((err) => {
-        res.status(500).send("Error -> " + err);
+
+    const queryBySexSect = ['sex', 'sector'];
+    const queryByContract = ['contract-type', 'modality'];
+
+    const queryBuilder = ((table, options) => {
+        table.findAll(options).then((accidents) => {
+            res.send(accidents);
+        }).catch((err) => {
+            res.status(500).send("Error -> " + err);
+        })
     })
+
+    if (queryBySexSect.includes(criteria)) {
+        const options = {
+            attributes: [
+                [db.sequelize.fn('SUM', db.sequelize.col('total')), 'total'],
+                'year',
+                'sex_id',
+                'sector_id',
+            ],
+            group: ['year', criteria],
+            order: ['year', criteria, 'sector_id', 'sex_id']
+        }
+        queryBuilder(accidentsBySexSect, options);
+    } else if (queryByqueryByContractSexSect.includes(criteria)) {
+        const options = {
+            attributes: [
+                [db.sequelize.fn('SUM', db.sequelize.col('total')), 'total'],
+                'year',
+                'contract_type_id',
+                'modality_id',
+            ],
+            group: ['year', criteria],
+            order: ['year', criteria]
+        }
+        queryBuilder(accidentsByContract, options);
+    }
+
+    
 };
